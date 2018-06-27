6eb508bdd61630607a8159f6076387a7047d20d4
donsizemore <don.sizemore@gmail.com>
2018-06-27 15:17:17 -0400

    don't need our own after all

--- rstudio.sh
+++ rstudio.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -d -p 3737:3838 -p 8686:8787 -v $(pwd):/home/rstudio -e ADD=shiny odumunc/rstudio
+docker run -d -p 3838:3838 -p 8787:8787 -v $(pwd):/home/rstudio -e ADD=shiny rocker/rstudio
