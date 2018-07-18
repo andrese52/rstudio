d0d746b6b2089d888e485ab87c564629c37dc8e9
donsizemore <don.sizemore@gmail.com>
2018-07-18 17:01:01 -0400

    add Jon's DFC demo

---
+++ DFCCyberCarpentryRShinyDemo/demoshinyproject/ShinyAppLaunch.R
@@ -0,0 +1,10 @@
+#install.packages("shiny")
+# SET THE WORKING DIRECTORY TO YOUR PATH BELOW WHERE YOU COPIED THE PROJECT
+#setwd("~/Dropbox/DFCCyberCarpentryRShinyDemo/DemoShinyProject")
+
+#library (shiny)
+
+#runApp ("DemoShinyHistApp", display.mode = "showcase")
+#runApp("DemoShinyDataViewApp")
+
+
